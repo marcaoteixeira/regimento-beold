20364da8349be03e9693aac34cb67d16b2e52738
Marco Antônio Teixeira <71968507+marcaoteixeira@users.noreply.github.com>
2022-11-27 19:24:05 -0300

    primeiro envio

---
+++ App.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+app.get("/",(req, res) => {
+
+    res.render("index");
+});
+
+app.get("/app",(req, res) => {
+    res.render("App.ejs");
+});
+
+app.listen(8080,()=> {console.log('App rodando');});
