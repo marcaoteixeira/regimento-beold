58cc3e5b6e6c0203e6ff91a0667e7c7d0414bff5
marcaoteixeira <marcao.teixeira@gmail.com>
2023-04-10 13:48:45 -0300

    Commit Dumps

--- Dumps/db_regdigital_tab_requerimento.sql
+++ Dumps/db_regdigital_tab_requerimento.sql
@@ -23,13 +23,13 @@ DROP TABLE IF EXISTS `tab_requerimento`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tab_requerimento` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `requerimento` varchar(100) NOT NULL DEFAULT ' ',
   `fundamento` varchar(100) NOT NULL DEFAULT ' ',
   `autor` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT ' ',
-  `obs` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT ' ',
+  `obs` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT ' ',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Tabela de requerimentos procedimentais.';
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Tabela de requerimentos procedimentais.';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,6 +38,7 @@ CREATE TABLE `tab_requerimento` (
 
 LOCK TABLES `tab_requerimento` WRITE;
 /*!40000 ALTER TABLE `tab_requerimento` DISABLE KEYS */;
+INSERT INTO `tab_requerimento` VALUES (1,'Adiamento da discussão','Art. 117, X; Art. 177','Líder/Autor/Relator','Nas proposições de tramitação com prioridade, 3 sessões; nas proposições de tramitação ordinária e nas proposições de emenda à Constituição, 5 sessões.'),(2,'Adiamento da discussão em matéria urgente','Art. 177, X; Art. 177, §1º','1/10 Deputados/Líderes','1/10 dos Deputados ou Líderes que representarem esse número, por 1 sessão.'),(4,'Adiamento da votação','Art. 117, X; Art. 193','Líder/Autor/Relator','Nas proposições e tramitação com prioridade,3 sessões; nas proposições de tramitação ordinária e nas propostas de emenda à Constituição, 5 sessões.'),(5,'Adiamento da Votaçao em matéria urgente','Art. 117, X; Art 193, §3º','1/10 Deputados/Líderes','1/10 dos Deputados ou Líderes que represemtem esse número, por 1 sessão'),(6,'Apensação/Desapensação','Art. 142','Deputado ou Comissão','Qualquer Deputado ou Comissão.'),(7,'Apreciação preliminar em Plenário relativa à PEC','Art. 202, §1º','Autor','Com apoio de Líderes que representem, mínimo, 1/3 dos Deputados.');
 /*!40000 ALTER TABLE `tab_requerimento` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-08 10:15:48
+-- Dump completed on 2023-04-10 13:47:50
--- Dumps/db_regdigital_tab_usopalavra.sql
+++ Dumps/db_regdigital_tab_usopalavra.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `tab_usopalavra`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tab_usopalavra` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `evento` varchar(100) NOT NULL DEFAULT '',
   `destinatario` varchar(100) NOT NULL DEFAULT '',
   `tempo` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-08 10:15:48
+-- Dump completed on 2023-04-10 13:47:50
